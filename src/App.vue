43567151e736c0c1247e9f24de42961b555dd435
Manuel Quero Delgado <manuel.querodelgado@gmail.com>
2022-03-30 23:50:52 +0200

    Una versión del fichero

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Bienvenido a su Aplicación Vue.js"/>
 </template>
 
 <script>
